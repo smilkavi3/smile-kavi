741a071dcf5ce9478fc637b88d9be169ded2d2b5
“[smilkavi3]” <[kaviyarasan010201@gmail.com]>
2024-05-16 12:23:37 +0530

    task

---
+++ task_OA.py
@@ -0,0 +1,35 @@
+# flams game:
+# name1=input("Enter name:").lower()
+# name2=input("Enter name:").lower()
+
+# def function(name1,name2):
+#  name1=input("Enter name:").lower()
+#  name2=input("Enter name:").lower()
+#  name1=name1.replace(" ","")
+#  name2=name2.replace(" ","")
+#  for i in name1:
+#     for j in name2:
+#         if i==j:
+#             name1=name1.replace(i,"",1)
+#             name2=name2.replace(j,"",1)
+#             break                   
+#  count=len(name1+name2)
+# #  print(count)
+#  if count>0:
+#   list=["f","l","a","m","s"]
+#   while len(list)>1:
+#     count=count%len(list)
+#     index=count-1
+#     if index>=0:
+#         right=list[:index]
+#         left=list[index+1:]
+#         new_list=right+left
+#     else:
+#         list[len(list)-1]
+#     print("relation  is: ",list[0])
+#     break
+#  else:
+#     print("different")
+# function("priya","ajay")
+    
+
