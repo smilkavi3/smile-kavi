56ca3dc78277e3970796ee900fd5fd66e4e7a9b2
“[smilkavi3]” <[kaviyarasan010201@gmail.com]>
2024-05-20 19:03:08 +0530

    update

--- task_OA.py
+++ task_OA.py
@@ -2,7 +2,7 @@
 # name1=input("Enter name:").lower()
 # name2=input("Enter name:").lower()
 
-# def function(name1,name2):
+# def function():
 #  name1=input("Enter name:").lower()
 #  name2=input("Enter name:").lower()
 #  name1=name1.replace(" ","")
@@ -12,24 +12,35 @@
 #         if i==j:
 #             name1=name1.replace(i,"",1)
 #             name2=name2.replace(j,"",1)
-#             break                   
+#             break  
+        
+                         
 #  count=len(name1+name2)
 # #  print(count)
 #  if count>0:
-#   list=["f","l","a","m","s"]
-#   while len(list)>1:
-#     count=count%len(list)
-#     index=count-1
-#     if index>=0:
-#         right=list[:index]
-#         left=list[index+1:]
-#         new_list=right+left
-#     else:
-#         list[len(list)-1]
+#     list=["f","l","a","m","e","s"]
+#     while len(list)>1:
+#       c=count%len(list)
+#       index=c-1
+#       if index>=0:
+#          left=list[:index]
+#          right=list[index+1:]
+#          list=right+left
+#       else:
+#          list=list[:len(list)-1]
 #     print("relation  is: ",list[0])
-#     break
 #  else:
 #     print("different")
-# function("priya","ajay")
+# function()
     
 
+a=142857
+c=[]
+n=6
+for i in range(1,n):
+    b=a*i
+    if (str(b) not in c) and (len(str(b))==6):
+        print("true is same digit ",b)
+    else:
+        print("false is not same digit ",b)
+print()
