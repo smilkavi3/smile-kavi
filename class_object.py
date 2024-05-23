cb879d2a2eb791bceff8ed45881a76e9943b575e
“[smilkavi3]” <[kaviyarasan010201@gmail.com]>
2024-05-23 18:47:55 +0530

    new_task

---
+++ class_object.py
@@ -0,0 +1,56 @@
+# class is blue print to creat object (car) and object is (output of car)
+# class consist of attributs,functionality
+# sir program:
+# class Student:
+#     # constructor
+#     def __init__(self, firstName, lastName, age, location, skills):
+#         # attribute
+#         self.firstName = firstName
+#         print(self)
+#         self.lastName = lastName
+#         self.age = age
+#         self.location = location
+#         self.skills = skills
+        
+  
+#     # functionality 
+#     def getDetails(self):
+#         print("Student Details")
+#         print("*" * 10)
+#         print("First Name", self.firstName)
+#         print("Last Name", self.lastName)
+#         print("Age", self.age)
+#         print("Location", self.location)
+#         print("Skills :")
+#         for i in self.skills:
+#             print(i)
+#         print("*" * 10, end="\n" * 2)
+        
+#     def getFullName(self):
+#         print(self.firstName + " " + self.lastName)
+    
+
+# #  a is object
+# a = Student(
+#     "kaviyarasan", "B.", 22, "Pondy", ["PYTHON", "C" , "JAVA script", "HTML", "CSS", "sass"]
+# )
+
+
+# a.getFullName()
+# a.getDetails()
+
+# b.getFullName()
+# b.getDetails()
+
+
+# def power(n,p):
+
+#   if p%2==0:
+#     a=power(n,p//2)
+#     return a*2
+   
+   
+# c=power(5,2)
+# print(c)
+
+
--- task_OA.py
+++ task_OA.py
@@ -1,4 +1,4 @@
-# flams game:
+# flams game using program :no:1
 # name1=input("Enter name:").lower()
 # name2=input("Enter name:").lower()
 
@@ -34,13 +34,74 @@
 # function()
     
 
-a=142857
-c=[]
-n=6
-for i in range(1,n):
-    b=a*i
-    if (str(b) not in c) and (len(str(b))==6):
-        print("true is same digit ",b)
-    else:
-        print("false is not same digit ",b)
+# a=142857
+# c=[]
+# n=6
+# for i in range(1,n):
+#     b=a*i
+#     if (str(b) not in c) and (len(str(b))==6):
+#         print("true is same digit ",b)
+#     else:
+#         print("false is not same digit ",b)
+# print()
+
+# a=input("enter version: ")
+# b=input("enter version: ")
+# n1=a.replace(".","")
+# n2=b.replace(".","")
+# # length1=len(n1)
+
+# # length2=len(n2)
+# # l1=[]
+# # l2=[]
+# # l1.append(n1)
+# # l2.append(n2)
+# # print(n1)
+# # # sum=0
+# # # sum+=a
+# # if length1>length2:
+# #     c.append(int(length1))
+# # else:
+# #     c.append(int(length2))
+    
+# # print(c)
+# sum1=0
+# for i in n1:
+#     sum1=sum1+int(i)
+# print()
+# sum2=0
+# for i in n2:
+#     sum2=sum2+int(i)
+#     # print(sum2)
+# total=sum1-sum2
+# # print(total)
+# if total>0:
+#     print("upgrade",a)
+# if 0>total:
+#     print("down grade",b)
+# if 0==total:
+#     print("equal",a,b)
+
+
+# find the version using program:no:2
+a=input("enter version: ")
+b=input("enter version: ")
+n1=a.replace(".","")
+n2=b.replace(".","")
+
+sum1=0
+for i in n1:
+    sum1=sum1+int(i)
 print()
+sum2=0
+for i in n2:
+    sum2=sum2+int(i)
+    # print(sum2)
+total=sum1-sum2
+# print(total)
+if total>0:
+    print("dow grade",a)
+if 0>total:
+    print("up grade",b)
+if 0==total:
+    print("equal",a,b)
